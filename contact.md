3939bc1d7680cba0f764a005131e70bb1ce8e443
Gopal Hegde <gplhegde@gmail.com>
2019-05-12 23:28:41 +0800

    new personal site

--- contact.md
+++
@@ -1,8 +0,0 @@
----
-layout: page
-title: Contact
-permalink: /contact/
----
-
-Email : gplhegde@gmail.com
-
---
+++ precache-manifest.842526f313b3d24fc75e7bf1d64800ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143c886db3833ce8c3267e5715a7e891",
+    "url": "/gplhegde/gplhegde.github.io/index.html"
+  },
+  {
+    "revision": "783190e3897c3d2ec166",
+    "url": "/gplhegde/gplhegde.github.io/static/css/2.1e871fd3.chunk.css"
+  },
+  {
+    "revision": "162c04ab9d24f949d718",
+    "url": "/gplhegde/gplhegde.github.io/static/css/main.62973313.chunk.css"
+  },
+  {
+    "revision": "783190e3897c3d2ec166",
+    "url": "/gplhegde/gplhegde.github.io/static/js/2.c512d921.chunk.js"
+  },
+  {
+    "revision": "162c04ab9d24f949d718",
+    "url": "/gplhegde/gplhegde.github.io/static/js/main.9eee7c97.chunk.js"
+  },
+  {
+    "revision": "03c479dd555090158be8",
+    "url": "/gplhegde/gplhegde.github.io/static/js/runtime~main.9ebff843.js"
+  },
+  {
+    "revision": "8c61a9a66a0b83689c551ff6aabccd98",
+    "url": "/gplhegde/gplhegde.github.io/static/media/pp.8c61a9a6.jpg"
+  }
+]);
